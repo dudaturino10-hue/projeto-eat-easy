e7a68507bedc7c155c517fb824ea6b11633155e8
Maria Gabriela Martins Lopes <mglopes@student.dei.uc.pt>
2025-11-26 11:42:07 +0000

    Descrição da alteração

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php
+p<?php
 require_once __DIR__ . '/php/pegaRestaurantes.php';
 require_once __DIR__ . '/php/filtroRestaurante.php';
 ?>
@@ -27,7 +27,7 @@ require_once __DIR__ . '/php/filtroRestaurante.php';
         <div class="cabecalho-opcoes">
             <a class="cabecalho-opcao" href="index.php">Início</a>
             <a class="cabecalho-opcao" href="./pages/historico-reservas.php">Minhas Reservas</a>
-            <a class="cabecalho-login" href="./pages/login-registo.php">Login/Registro</a>
+            <a class="cabecalho-login" href="./pages/login-registo.php">Login/RegistroASGSGD</a>
             <!---Ancora serve para levar a outra pagina do próprio site.-->
         </div>
     </div>
